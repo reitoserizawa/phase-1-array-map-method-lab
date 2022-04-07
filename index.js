e0ac83ead9a8ced28b83febbde73015bbf8665c2
ReitoSerizawa <s.reitiger@gmail.com>
2022-04-07 12:15:48 -0400

    commit tree 14645d592a13cb93ba5975d6ca5a4ad3d2227822

--- index.js
+++ index.js
@@ -12,5 +12,46 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
-}
+  return tutorials.map((eachTutorial) => {
+    const words = eachTutorial.split(" ");
+    const capitalizeMe = words.map((word) => word.substring(0, 1).toUpperCase() + word.substring(1));
+    const whatever = capitalizeMe.join(" ")
+    return whatever;}
+    );
+};
+
+// const titleCased = () => {
+//   return tutorials.map((line) => {
+//     const tokens = line.split(" ");
+//     const capitalizedTokens = tokens.map(
+//       (token) => token.charAt(0).toUpperCase() + token.slice(1)
+//     );
+//     const response = capitalizedTokens.join(" ");
+//     return response;
+//   });
+// };
+
+titleCased();
+
+// const titleCased = () => {
+//   return tutorials.map (eachTask => {
+//     let eachWord = eachTask.split (" ")
+//     eachWord.map ((letter) => letter[0].toUpperCase() + letter.substring(1)).join(" ")
+//     return eachWord
+//     })
+// }
+
+// titleCased()
+
+
+// // return tutorialsUpper = tutorials.map (whatever => {
+// //   return whatever.toUpperCase()
+// // })
+
+// // const activatedRobots = robots.map ((robot) => {
+//   // return Object.assign({}, robot, {isActivated: true}) } )
+
+
+// // titleCased(): returns an array with title case tutorial names. 
+// // Note that this function takes 
+// // no arguments and should use the global tutorials variable as data.
